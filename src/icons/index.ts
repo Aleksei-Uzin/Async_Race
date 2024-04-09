8227faa05b33ccfbf776036046549a2646d8cf90
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-04-09 22:31:33 +0400

    feat: add close icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,6 +1,7 @@
 import ArrowLeft from './svgs/arrowLeft.svg'
 import ArrowRight from './svgs/arrowRight.svg'
 import Car from './svgs/car.svg'
+import Close from './svgs/close.svg'
 import Gear from './svgs/gear.svg'
 
 import { SVGComponent } from './types'
@@ -8,4 +9,5 @@ import { SVGComponent } from './types'
 export const IconArrowLeft: SVGComponent = ArrowLeft
 export const IconArrowRight: SVGComponent = ArrowRight
 export const IconCar: SVGComponent = Car
+export const IconClose: SVGComponent = Close
 export const IconGear: SVGComponent = Gear
