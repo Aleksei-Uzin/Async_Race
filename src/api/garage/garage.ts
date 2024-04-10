4625f8b6a912ee40d36b38756dc174bc8bdd0821
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-04-10 14:53:36 +0400

    feat: add garage api contacts

--- src/api/garage/garage.ts
+++ src/api/garage/garage.ts
@@ -1,10 +1,10 @@
 import { IPagination, fetchPagelable } from '../fetchPagelable'
-import { PAGE_SIZE } from '../constants'
+import { PAGE_SIZE, START_PAGE } from '../constants'
 import { ICar } from './types'
-import { prepareQueryParams } from '../utils'
+import { getFullUrl, prepareQueryParams } from '../utils'
 
 export const getCars = async ({
-  page = 1,
+  page = START_PAGE,
 }: {
   page?: number
 }): Promise<IPagination<ICar>> => {
@@ -12,4 +12,43 @@ export const getCars = async ({
   const cars = await fetchPagelable<ICar>('/garage', page, queryParams)
 
   return cars
+}
+
+export const createCar = async ({
+  color,
+  name,
+}: Omit<ICar, 'id'>): Promise<ICar> => {
+  const fullUrl = getFullUrl('/garage')
+  const response = await fetch(fullUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ color, name }),
+  })
+  const content: ICar = await response.json()
+
+  return content
+}
+
+export const deleteCar = async (id: number): Promise<void> => {
+  const fullUrl = getFullUrl('/garage')
+  const urlWithId = `${fullUrl}/${id}`
+  await fetch(urlWithId, { method: 'DELETE' })
+}
+
+export const updateCar = async ({ id, ...payload }: ICar): Promise<ICar> => {
+  const fullUrl = getFullUrl('/garage')
+  const urlWithId = `${fullUrl}/${id}`
+  const response = await fetch(urlWithId, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+
+  const content: ICar = await response.json()
+
+  return content
 }
